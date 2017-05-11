8b58c350e85d4bd3b5136add8f30ec70628b5dd7
hepiska <ego1403@gmail.com>
2017-05-11 14:52:36 +0700

    ubah config

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+module.exports = {
+  "development": {
+    "username": "ego",
+    "password": "ego",
+    "database": "buahtangan",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "logging":false
+  },
+  "test": {
+    "username": "ego",
+    "password": "ego",
+    "database": "buahtangan_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.RDS_USERNAME || '',
+    "password": process.env.RDS_PASSWORD || '',
+    "database": process.env.RDS_DB_NAME || 'bukalelang-db',
+    "host": process.env.RDS_HOSTNAME || '',
+    "port": process.env.RDS_PORT || '5432',
+    "dialect": "postgres",
+    "logging":false
+  }
+}
