9bb48ba5c63ed50e7901984b8fbf4ce893b047a6
hepiska <ego1403@gmail.com>
2017-05-13 18:05:09 +0700

    app

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const api = require('./routers/api');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 app.use(cors());
 app.use('/api', api);
 
