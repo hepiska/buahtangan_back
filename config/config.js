26f57597b61641d16cc9e53b69c93f134ab78283
hepiska <ego1403@gmail.com>
2017-05-17 13:30:43 +0700

    data for travis

--- config/config.js
+++ config/config.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": "ego",
-    "password": "ego",
-    "database": "buahtangan",
-    "host": "127.0.0.1",
+    "username": "jvwrmmzwcpqyrd",
+    "password": "382cfd844cd6a50a22abbca39777409ac54a2e0dbf36965665aa964376343e81",
+    "database": "d8ano12tlk1j6l",
+    "host": "ec2-54-235-120-39.compute-1.amazonaws.com",
     "dialect": "postgres",
-    "logging":false
+    "logging": false,
+    "dialectOptions":{
+      "ssl":{
+       "require":true
+       }
+}
   },
   "test": {
     "username": "ego",
